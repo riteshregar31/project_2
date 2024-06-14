e05d0a349b94a9befb926999a067eff3096f84f8
riteshregar31 <riteshregar31@gmail.com>
2024-06-14 11:16:26 +0530

    error handling and flash

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express=require('express');
 const app=express();
+
 const path=require('path')
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const mongoose=require('mongoose');
 const Userinfo=require('./models/Userinfo');
 const methodOverride = require('method-override');
-const { userInfo } = require('os');
+// const { userInfo } = require('os');
+const flash = require('connect-flash');
 app.use(methodOverride('_method'));
+const ejsMate=require('ejs-mate');
+const session = require('express-session');
+const secret = process.env.SECRET || 'thisdfdssdfdsfdstet!';
 const dburl='mongodb://127.0.0.1:27017/showmanager'
 app.use(express.urlencoded({ extended: true }));
+
 main().catch(err => console.log(err));
 
 
@@ -27,8 +35,44 @@ const db=mongoose.connection;
 // app.use(express.urlencoded({ extended: true }));
 // app.use(methodOverride('_method'));
 app.set('view engine','ejs')
+app.engine('ejs',ejsMate)
 app.set('views',path.join(__dirname,'views'))
+app.use(express.static(path.join(__dirname, 'public')))
 // app.use(express.static(path.join(__dirname, 'public')))
+const sessionConfig = {
+  
+  name: 'session',
+  secret,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+      httpOnly: true,
+      // secure: true,
+      expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7
+  }}
+
+app.use(session(sessionConfig))
+app.use(flash());
+
+
+
+
+
+
+
+app.use((req, res, next) => {
+  console.log(req.session)
+  res.locals.currentUser = req.user;
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+})
+
+
+
+
+
 
 
 app.get('/',(req,res)=>{
@@ -50,7 +94,7 @@ res.render('shows/newuser')
 
 
 
-app.post('/userinfo', (async (req, res, next) => {
+app.post('/userinfo', catchAsync(async (req, res, next) => {
   const hr = new Userinfo(req.body.sr);
     // console.log(hr);
     await hr.save();
@@ -61,7 +105,7 @@ app.post('/userinfo', (async (req, res, next) => {
 
 
 
-app.get('/userinfo/:id',async(req,res)=>{
+app.get('/userinfo/:id',catchAsync(async(req,res)=>{
   const {id}=req.params;
   const sr=await Userinfo.findById(id);
 //   var idd={idno:'565989623265'};
@@ -72,24 +116,24 @@ app.get('/userinfo/:id',async(req,res)=>{
 //     { $push: { showid: idd  } },
 //  );
  
-
+req.flash('success', 'Welcome back!!! ')
   res.render('./shows/showuser',{sr})
-})
+}))
 
 
 
 
-app.get('/userinfo/:id/editp',async (req,res)=>{
+app.get('/userinfo/:id/editp',catchAsync(async (req,res)=>{
   const {id}=req.params;
   const sr=await Userinfo.findById(id);
   res.render('./shows/editp',{sr})
-})
+}))
 
 
 
 
 
-app.get('/delete/wishlist/:id/:showid',async (req,res)=>{
+app.get('/delete/wishlist/:id/:showid',catchAsync(async (req,res)=>{
   const {id,showid}=req.params;
   var idd={idno:showid};
 const sr=await Userinfo.findById(id);
@@ -98,13 +142,13 @@ await  Userinfo.findOneAndUpdate(
   { $pull: { showid: idd  } },
 );
 res.send('sfsfd')
-})
+}))
 
 
 
 
 
-app.put('/userinfo/:id', async(req, res) => {
+app.put('/userinfo/:id', catchAsync(async(req, res) => {
   const { id } = req.params;
 
 //   if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -122,13 +166,13 @@ app.put('/userinfo/:id', async(req, res) => {
    const sr = await Userinfo.findByIdAndUpdate(id, { ...req.body.sr });
   res.redirect(`/userinfo/${sr._id}`);
   
-});
+}));
 
 
 
 
 
-app.get('/userinfo/:id/add/:showid',async(req,res)=>{
+app.get('/userinfo/:id/add/:showid',catchAsync(async(req,res)=>{
 const {id,showid}=req.params;
 console.log(showid)
 var idd={idno:showid};
@@ -141,37 +185,42 @@ await  Userinfo.findOneAndUpdate(
 // res.render('./shows/showuser',{sr})
 res.redirect(`/userinfo/${sr._id}`)
 
-})
+}))
 
 
 
 
 
-app.get('/showinfo/:id',(req,res)=>{  
+app.get('/showinfo/:id',catchAsync((req,res)=>{  
     const {id}=req.params;
     // console.log(id)
     // const rest= axios.get(` https://api.tvmaze.com/shows/${id}`)
     // console.log(res)
     
     res.render('shows/show',{id});
-  })
+  }))
 
 
 
 
-  app.get('/seasons/:id/episodes',(req,res)=>{
+  app.get('/seasons/:id/episodes',catchAsync((req,res)=>{
 
     const {id}=req.params;
     res.render('shows/episodes',{id})
-  })
+  }))
 
 
 
 
-  app.get('/people/:id',(req,res)=>{
+  app.get('/people/:id',catchAsync((req,res)=>{
     const {id}=req.params;
     res.render('shows/person',{id})
+  }))
+
+  app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
   })
+  
 
   app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
